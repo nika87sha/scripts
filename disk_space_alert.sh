0c5896e100496d48698a2d77a52d2a44e9b3d35d
Veronica Duran <veronica.duran87@proton.me>
2024-04-12 14:35:26 +0200

    update disk_space_alert

--- disk_space_alert.sh
+++ disk_space_alert.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-threshold=90
+
+threshold=50
 current_usage=$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')
+
 if [ "$current_usage" -ge "$threshold" ]; then
-	echo "Disk space is running low!"
+	echo "Disk space is running low!" | mail -s "Alert: Low disk space" admin@example.com
 else
 	echo "Disk space is okay"
 fi
