9ad59aaede66b65e58ff892e09028264c4235d70
Verónica Durán <veronica.duran87@proton.me>
2026-07-26 18:25:36 +0200

    fix: change ssh select fzf

--- ssh-select.sh
+++ ssh-select.sh
@@ -4,10 +4,10 @@
 # Extrae todos los hosts definidios (evita comodines como "Host *")
 # También descompone múltiples alias en una sola linea (Host dev1 dev2)
 
-host=$(awk '/^Host / { for (i=2; i<=NF; i++) if ($i != "*") print $i }' ~/.ssh/config |
-	fzf --prompt="SSH Hosts > " \
-		--preview='grep -A 5 -w "Host {}" ~/.ssh/config' \
-		--height=40% --layout=reverse --border)
+host=$(awk '/^Host / { for (i=2; i<=NF; i++) if ($i != "*") print $i }' ~/.ssh/config \
+  | fzf --prompt="SSH Hosts > " \
+    --preview='grep -A 5 -w "Host {}" ~/.ssh/config' \
+    --height=40% --layout=reverse --border)
 
 # Si se seleccionó un host, lanza la conexión
 [ -n "$host" ] && ssh "$host"
